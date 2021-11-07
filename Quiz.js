5adb488bd46df5f13d29f847102ca6b42dd46134
Guru706 <93817268+Guru706@users.noreply.github.com>
2021-11-07 08:19:34 -0800

    Update Quiz.js

--- Quiz.js
+++ Quiz.js
@@ -1,20 +1,40 @@
 <script>
-document.write("Welcome to the game");
 
-var ask=window.prompt("Enter yes to continue the game and enter no to quit the game:");
+prompt("Enter your name");
+
+var a=window.prompt('Age');
+
+confirm('Confirm if you want to play the quiz');
+
+
+var ask=window.prompt("Welcome to the game! Enter yes to continue the game and enter no to quit the game:");
 
 if(ask=="yes"||ask=="Yes"||ask=="YES") {
-		document.write(' Welcome to first level.');
-        var one=window.prompt('How many toes does human have:');
+		var one=window.prompt('Welcome to first level. How many toes does human have:');
         if(one==10) {
-        var one=0;
-        one++;
-    	  document.write(" You have ", one , " point ");
-		    }else{
-         document.write("Incorrect");
+        confirm("correct");
+		}else{
+        confirm("Quiz one answer is Incorrect");
+        }
+		var two=window.prompt("What is the national language of Korea?");
+        if(two=='korean'||two=='KOREAN'||two=='Korean'){
+        confirm("Correct");
+       	}else{ 
+        confirm(" Quiz two answer is Incorrect");
+        }
+        var three=window.prompt('How many oceans are there in world?');
+        if(three==7){
+        confirm("correct");
+        }else{
+        confirm('Incorrect');
         }
         
+        confirm('Goodbye');
+
+
 }else{
-document.write("Goodbye");
+confirm(" Goodbye ");
 }
+
+
 </script>
